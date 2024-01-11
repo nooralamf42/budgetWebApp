fcfccb1be5a32daf9a4e253b7741f77700c82d4f
nooralamf42 <nooralamf42@gmail.com>
2024-01-11 22:27:47 +0530

    adding local storage

--- app.js
+++ app.js
@@ -1,32 +1,67 @@
-let moneyIncome = new Audio("https://cdn.pixabay.com/download/audio/2022/12/17/audio_43e9af63f3.mp3?filename=cashier-quotka-chingquot-sound-effect-129698.mp3")
-let moneyDeduction = new Audio("https://cdn.pixabay.com/download/audio/2022/03/10/audio_18fa31cc65.mp3?filename=coin-drop-39914.mp3")
-let amount = (amountValue) => {
-    moneyIncome.pause()
-    moneyIncome.currentTime = 0;
-    moneyDeduction.pause()
-    moneyDeduction.currentTime = 0;
-    if(amountInput.value !=""){let borderDiv = document.createElement('div')
-    div2.classList.remove('d-none')
-    dataDiv.appendChild(borderDiv)
-    borderDiv.classList.add("border", "mb-3")
-    let dataH3 = document.createElement("h3")
-    dataH3.classList.add('border-4' ,'border-end', 'p-3', 'm-0')
-    borderDiv.appendChild(dataH3)
-    console.log(dataH3)
-    if (amountValue) {
-        moneyIncome.play();
-        balance.innerText = parseFloat(balance.innerText) + parseFloat(amountInput.value)
-        income.innerText = parseFloat(income.innerText) + parseFloat(amountInput.value)
-        dataH3.classList.add('border-success')
-        dataH3.innerHTML = `${description.value} <span class='text-success'>+${amountInput.value}</span>`
+let moneyIncome = new Audio(
+  "https://cdn.pixabay.com/download/audio/2022/12/17/audio_43e9af63f3.mp3?filename=cashier-quotka-chingquot-sound-effect-129698.mp3"
+);
+let moneyDeduction = new Audio(
+  "https://cdn.pixabay.com/download/audio/2022/03/10/audio_18fa31cc65.mp3?filename=coin-drop-39914.mp3"
+);
+
+const upDateBalance = (balance, newMoney, name, isExpense)=>{
+  let ls = {
+    amount : 5454,
+    history : [
+      {
+        'expenseName': "name here",
+        'value' : 'value',
+        'isExpense' : false
       }
-      else{
-        moneyDeduction.play()
-        balance.innerText = parseFloat(balance.innerText) - parseFloat(amountInput.value)
-        expense.innerText = parseFloat(expense.innerText) + parseFloat(amountInput.value)
-        dataH3.classList.add('border-danger')
-        dataH3.innerHTML = `${description.value} <span class='text-danger'>-${amountInput.value}</span>`
-      }}
-      amountInput.value = ""
-      description.value = ""
+    ],
+  }
+  console.log(balance, newMoney, name ,isExpense)
+}
+
+
+
+let amount = (amountValue) => {
+  moneyIncome.pause();
+  moneyIncome.currentTime = 0;
+  moneyDeduction.pause();
+  moneyDeduction.currentTime = 0;
+  if (amountInput.value != "") {
+    let borderDiv = document.createElement("div");
+    div2.classList.remove("d-none");
+    dataDiv.appendChild(borderDiv);
+    borderDiv.classList.add("border", "mb-3");
+    let dataH3 = document.createElement("h3");
+    dataH3.classList.add("border-4", "border-end", "p-3", "m-0");
+    borderDiv.appendChild(dataH3);
+    console.log(dataH3);
+
+    if (amountValue) {
+      moneyIncome.play();
+      balance.innerText =
+        parseFloat(balance.innerText) + parseFloat(amountInput.value);
+      income.innerText =
+        parseFloat(income.innerText) + parseFloat(amountInput.value);
+      dataH3.classList.add("border-success");
+
+      upDateBalance(balance.innerText, description.value, amountInput.value, false)
+
+      dataH3.innerHTML = `${description.value} <span class='text-success'>+${amountInput.value}</span>`;
+
+    } else {
+      moneyDeduction.play();
+      balance.innerText =
+        parseFloat(balance.innerText) - parseFloat(amountInput.value);
+      expense.innerText =
+        parseFloat(expense.innerText) + parseFloat(amountInput.value);
+      dataH3.classList.add("border-danger");
+
+      upDateBalance(balance.innerText, description.value, amountInput.value, true)
+
+
+      dataH3.innerHTML = `${description.value} <span class='text-danger'>-${amountInput.value}</span>`;
     }
+  }
+  amountInput.value = "";
+  description.value = "";
+};
